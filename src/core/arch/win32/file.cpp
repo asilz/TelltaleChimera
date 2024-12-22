20a2cf19f8a4eb2ce50a8d96492bfc49f6470885
Asil Zogby <asilz@stud.ntnu.no>
2024-12-22 23:38:42 +0100

    Update windows file dialog

--- src/core/arch/win32/file.cpp
+++ src/core/arch/win32/file.cpp
@@ -1,10 +1,11 @@
-#include <cstdio>
 #include <Windows.h>
+#include <cstdio>
 
-int FileBrowsePath(char *buf, size_t maxLength) {
+int FileBrowsePath(char *buf, size_t maxLength)
+{
     OPENFILENAME open = {0};
     open.lStructSize = sizeof(OPENFILENAME);
-    open.lpstrFilter = "*.skl;*.anm;*.d3dmesh\0\0";
+    open.lpstrFilter = ".skl, .anm or .d3dmesh\0*.skl;*.anm;*.d3dmesh\0";
     open.lpstrFile = buf;
     open.lpstrFile[0] = '\0';
     open.nMaxFile = maxLength;
@@ -14,15 +15,16 @@ int FileBrowsePath(char *buf, size_t maxLength) {
     open.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_EXPLORER;
     return GetOpenFileName(&open);
 }
-int FileSavePath(char* buf, size_t maxLength) {
+int FileSavePath(char *buf, size_t maxLength)
+{
     OPENFILENAME open = {0};
     open.lStructSize = sizeof(OPENFILENAME);
-    open.lpstrFilter = "*.skl;*.anm;*.d3dmesh\0\0";
+    open.lpstrFilter = ".glb\0*.glb\0";
     open.lpstrFile = buf;
     open.lpstrFile[0] = '\0';
     open.nMaxFile = maxLength;
     open.nFilterIndex = 1;
-    open.lpstrTitle = "Select a .skl, .anm or .d3dmesh file\0";
+    open.lpstrTitle = "Save a .glb file\0";
     open.nMaxFileTitle = strlen(open.lpstrTitle);
     open.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_EXPLORER;
     return GetSaveFileName(&open);
